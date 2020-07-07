cfaa1e3431065a5c2477307e342f32bb091013ef
Trong Cong Nguyen <tronginc@gmail.com>
2020-07-07 21:00:40 +0700

    loader

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,9 +14,32 @@ $(document).ready(function () {
     })
 
     $('.slick-carousel').slick(getSlickSettings());
+    let resizing = false;
+    let to = undefined;
     $(window).on("resize", function () {
-        $('.slick-carousel').slick('unslick').slick(getSlickSettings());
+        resizing = true;
+        if (to) {
+            clearTimeout(to);
+            console.log("Clear");
+            to = setTimeout(function () {
+                $('.slick-carousel').slick('unslick').slick(getSlickSettings());
+                clearTimeout(to);
+                to = undefined;
+                console.log("Reinit slick");
+            }, 100)
+        }
+        else {
+            to = setTimeout(function () {
+                $('.slick-carousel').slick('unslick').slick(getSlickSettings());
+                clearTimeout(to);
+                to = undefined;
+                console.log("Reinit slick");
+            }, 100)
+        }
     });
+
+    $("#loader-container").remove();
+    $("body").css({ overflow: "auto" });
 })
 
 function getSlickSettings() {
