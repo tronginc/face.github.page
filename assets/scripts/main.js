3051976afb073dcbd02fe6bc689f63d34ff77f79
Trong Cong Nguyen <tronginc@gmail.com>
2020-07-07 20:23:16 +0700

    mobile menu

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+    const menu_wrapper = $('#menu-wrapper-mb');
+    const menu_list = $('#menu-list');
+
+    menu_wrapper.height(0);
+    $("#menu").click(function () {
+        if (menu_wrapper.hasClass('open')) {
+            menu_wrapper.removeClass('open');
+            menu_wrapper.height(0);
+        } else {
+            menu_wrapper.addClass('open');
+            menu_wrapper.height(menu_list.outerHeight(true));
+        }
+    })
+
     $('.slick-carousel').slick(getSlickSettings());
     $(window).on("resize", function () {
         $('.slick-carousel').slick('unslick').slick(getSlickSettings());
